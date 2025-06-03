5d528be7afebb43a27fc3e23ff33bcdca2661084
Mike <mbaron999@gmail.com>
2025-06-03 15:47:11 -0600

    Implement dynamic plugin loading based on config.json plugins array

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import config from "../config.json";
 import AliceApp from "../core/AliceApp.tsx";
-import samplePlugin from "../plugins/sample-plugin/index.tsx";
 
-const plugins = [samplePlugin];
+async function loadPlugins(pluginNames: string[]) {
+  const pluginModules = await Promise.all(
+    pluginNames.map(name => import(`../plugins/${name}/index.tsx`))
+  );
+  return pluginModules.map(mod => mod.default);
+}
 
 export default function App() {
+  const [plugins, setPlugins] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    loadPlugins(config.plugins || []).then(setPlugins);
+  }, []);
+
+  if (!plugins.length) return <div>Loading plugins...</div>;
+
   return (
     <AliceApp config={config} plugins={plugins} />
   );
